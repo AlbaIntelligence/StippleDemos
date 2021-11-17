33fa04d7a1a776a3ae106bec2260bc0266b9cf14
Alba Intelligence <alba.intelligence@gmail.com>
2021-11-17 10:08:15 +0100

    Renaming

---
+++ BasicExamples/run_me_line_by_line.jl
@@ -0,0 +1,31 @@
+
+# Those lines are to make sure that the current path is in the package
+pwd()
+cd("BasicExamples")
+pwd()
+
+using Pkg
+Pkg.activate(".")
+
+# Use the latest versions of the necessary packages
+Pkg.develop("Genie")
+Pkg.develop("Stipple")
+Pkg.develop("StippleUI")
+Pkg.develop("StippleCharts")
+
+using Revise
+
+using Genie, Genie.Renderer.Html
+using Stipple
+using StippleUI
+using StippleCharts
+
+using BasicExamples
+push!(Base.modules_warned_for, Base.PkgId(BasicExamples))
+BasicExamples.main()
+
+
+# Create a server presenting the card
+# top webpage is a welcome page
+# the demos are on separate addresses given by their respective routes
+BasicExamples_server = create_server(; async = true, verbose=true, open_browser = true)
